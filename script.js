33145dbabafcff7cb8a059de9ceece393e63e719
Alexander Kozlov <alex-k@akozlov.lan>
2024-02-01 17:59:06 -0500

    Finished all functinality

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const resultText = document.getElementById('result');
+const scoreText = document.getElementById('score');
+
+let playerScore = 0;
+let computerScore = 0;
 
 rock.addEventListener('click', () => {
     resultText.innerHTML = playRound('rock');
@@ -18,6 +22,7 @@ scissors.addEventListener('click', () => {
 function playRound(playerSelection) {
     const computerSelection = computerPlay();
     const result = getResult(playerSelection, computerSelection);
+    scoreCheck(result);
     return result;
 }
 
@@ -36,4 +41,25 @@ getResult = (playerSelection, computerSelection) => {
     } else {
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
+}
+scoreCheck = (result) => {
+    if (result.includes('win')) {
+        playerScore++;
+    } else if (result.includes('lose')) {
+        computerScore++;
+    }
+    scoreText.innerHTML = `Player: ${playerScore} Computer: ${computerScore}`;
+    if (playerScore === 5) {
+        alert('You won the game!');
+        resetScore();
+    } else if (computerScore === 5) {
+        alert('You lost the game!');
+        resetScore();
+    }
+}
+resetScore = () => {
+    playerScore = 0;
+    computerScore = 0;
+    resultText.innerHTML = '';
+    scoreText.innerHTML = `Player: ${playerScore} Computer: ${computerScore}`;
 }
